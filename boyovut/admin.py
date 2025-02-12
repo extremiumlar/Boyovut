0adc33f122c1c41170d88d0af8cc6038a441ae0a
extremiumlar <nurillo1910@gmail.com>
2025-02-12 12:57:31 +0500

    To'garaklar modeli qo'shildi
    to'garaklar modeli qo'shildi

--- boyovut/admin.py
+++ boyovut/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
-
+from .models import Togarak_turlari,Togaraklar
 # Register your models here.
+
+@admin.register(Togaraklar)
+class TogaraklarAdmin(admin.ModelAdmin):
+    list_display = ['nomi','turi','davomiyligi','publish_time','active']
+    list_filter = ['turi','active','publish_time','boshlanish_sanasi']
+    #prepopulated_fields = {'slug': ('title',)}
+    date_hierarchy = 'boshlanish_sanasi'
+    search_fields = ['nomi']
+    ordering = ['active','publish_time']
+@admin.register(Togarak_turlari)
+class Togarak_turlariAdmin(admin.ModelAdmin):
+    list_didplay = ['id','name']
--- boyovut/models.py
+++ boyovut/models.py
@@ -1,3 +1,94 @@
-from django.db import models
+from django.utils import timezone
 
+from django.db import models
+from django.urls import reverse
 # Create your models here.
+
+class Togarak_turlari(models.Model):
+    name = models.CharField(max_length=100)
+    def __str__(self):
+        return self.name
+    class Meta:
+        verbose_name_plural = 'Yonalish turlari'
+
+class Togaraklar(models.Model):
+    nomi = models.CharField(max_length=100)
+    image = models.ImageField(upload_to='togarak_images')
+    turi = models.ForeignKey(Togarak_turlari, on_delete=models.CASCADE)
+    manzil = models.CharField(max_length=150)
+    oquvchilar_soni = models.IntegerField(blank=True,null=True)
+    davomiyligi = models.CharField(max_length=150,blank=True,null=True)
+    boshlanish_sanasi = models.DateField(default=timezone.now)
+    body = models.TextField()
+    body_small = models.TextField(max_length=250)
+    active = models.BooleanField(default=True)
+    publish_time = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.nomi
+    def get_absolute_url(self):
+        pass
+        # return reverse("yonalish_detail", kwargs={"slug": self.slug})
+    class Meta:
+        ordering = ['-publish_time']
+        verbose_name_plural = "Tog'araklar"
+
+class Yangilik_va_tadbirlar(models.Model):
+    nomi = models.CharField(max_length=100)
+    image = models.ImageField(upload_to='togarak_images')
+    manzil = models.CharField(max_length=150)
+    boshlanish_sanasi = models.DateField(default=timezone.now)
+    body = models.TextField()
+    body_small = models.TextField(max_length=250)
+    active = models.BooleanField(default=True)
+    publish_time = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.nomi
+    def get_absolute_url(self):
+        pass
+        # return reverse("yonalish_detail", kwargs={"slug": self.slug})
+    class Meta:
+        ordering = ['-publish_time']
+        verbose_name_plural = "Tog'araklar"
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
